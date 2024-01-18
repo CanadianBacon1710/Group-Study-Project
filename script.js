a6b9e8138a5f92075b2a1496f93978159d08d3c3
CanadianBacon1710 <146001580+CanadianBacon1710@users.noreply.github.com>
2024-01-18 15:03:07 +0000

    Thing

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//Toggle Button for light and dark mode
 
+function toggleDarkMode() {
+  const body = document.querySelector("body");
+  const darkModeButton = document.querySelector("#dark-mode-button");
+
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    darkModeButton.innerText = "🌚 Dark Mode";
+  } else {
+    darkModeButton.innerText = "🌞 Light Mode";
+  }
+}
